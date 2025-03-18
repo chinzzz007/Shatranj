d5a1daadcfd6d06643bf8479fa6d149cf0944959
Chinzzz <chinz.cm7@gmail.com>
2025-03-18 23:25:38 +0530

    Configured auth guard along with signout feature

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,32 @@
-import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { ChangePasswordDto } from './dto/changePassword.dto';
-import { Request} from 'express';
+import { Request, Response} from 'express';
+import { ReturnResponse } from 'src/auth/interfaces/return-response.interface';
 
 @UseGuards(AuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @HttpCode(201)
   @Post('change_password')
-  async changePassword(@Body() changePasswordData: ChangePasswordDto, @Req() req:Request){
+  async changePassword(@Body() changePasswordData: ChangePasswordDto, @Req() req:Request): Promise<ReturnResponse>{
     const user_id = req['user_id'];
     return this.usersService.changePassword(changePasswordData, user_id);
   }
 
-  // @Get('user_signout')
-  // async userSignout(@Req() req: Request, @Res({passthrough: true}) res: Response){
-  //   const refresh_token = req.cookies['refresh_token'];
-  //   const access_token = req.cookies['access_token'];
-  // }
+  // Signing out from the user's current device not all
+  @HttpCode(200)
+  @Get('user_signout')
+  async userSignout(@Req() req: Request, @Res({passthrough: true}) res: Response): Promise <ReturnResponse>{
+    res.clearCookie('refresh_token')
+    res.clearCookie('access_token')
+    return {
+      success: true,
+      status_code: 200,
+      message: "User signed out!"
+    }
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,30 +4,50 @@ import { Model, ObjectId } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { ChangePasswordDto } from './dto/changePassword.dto';
 import * as bcrypt from 'bcrypt';
+import { ReturnResponse } from 'src/auth/interfaces/return-response.interface';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-    async changePassword(changePasswordData: ChangePasswordDto, user_id: ObjectId){
-        const {old_password, new_password} = changePasswordData;
-        const existingUser = await this.userModel.findOne({_id: user_id});
+    async changePassword(changePasswordData: ChangePasswordDto, user_id: ObjectId): Promise<ReturnResponse>{
+        try{
+            const {old_password, new_password} = changePasswordData;
+            const existingUser = await this.userModel.findOne({_id: user_id});
 
-        if(!existingUser){
-            throw new UnauthorizedException('User not found please signup');
+            if(!existingUser){
+                throw new UnauthorizedException({
+                    success: false,
+                    status_code: 401,
+                    message: 'User not found please signup'
+                });
+            }
+
+            // comparing the passwords
+            const passwordMatch = await bcrypt.compare(old_password, existingUser.password);
+            if(!passwordMatch){
+                throw new BadRequestException({
+                    success: false,
+                    status_code: 400,
+                    message: "Invalid credentials!"
+                })
+            }
+
+            const new_hashed_password = await bcrypt.hash(new_password, 10);
+
+            const newUser = await this.userModel.updateOne(
+                {_id: user_id},
+                {$set: {password: new_hashed_password}}
+            );
+
+            return {
+                success: true,
+                status_code: 200,
+                message: "Password changed successfully!"
+            }
         }
-
-        // comparing the passwords
-        const passwordMatch = await bcrypt.compare(old_password, existingUser.password);
-        if(!passwordMatch){
-            throw new BadRequestException('Invalid credentials!')
+        catch(error){
+            throw error;
         }
-
-        const new_hashed_password = await bcrypt.hash(new_password, 10);
-
-        const newUser = await this.userModel.updateOne(
-            {_id: user_id},
-            {$set: {password: new_hashed_password}}
-        );
     }
 }
